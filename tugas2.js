24ea750cab30a5fe268c65aae7b83420b8fc0824
AmbisProgramming <110365940+AmbisProgramming@users.noreply.github.com>
2022-08-01 12:46:46 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+let string = "Ini String ";
+let number = 213;
+let boolean = true;
+let ininull = null;
+let undefined;
+let array = ["ini", "adalah", "array"];
+
+console.log(string);
+console.log(number);
+console.log(boolean);
+console.log(ininull);
+console.log(undefined);
+console.log(array);
